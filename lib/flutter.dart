04773915f3fa6015603be6eecbd393faf704b188
Matthew Hurst <matt@workpail.com>
2023-07-31 15:22:12 -0400

    Add new files to exports

--- lib/flutter.dart
+++ lib/flutter.dart
@@ -4,5 +4,7 @@ export 'core.dart';
 
 export 'src/flutter/app.dart';
 export 'src/flutter/adaptive_app.dart';
+export 'src/flutter/context.dart';
 export 'src/flutter/colors.dart';
 export 'src/flutter/router.dart';
+export 'src/flutter/text.dart';
