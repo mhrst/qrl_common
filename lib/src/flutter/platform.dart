fc0bda4c105808eeb5d9688eaeb49db51045b454
Matthew Hurst <matt@workpail.com>
2023-09-26 23:35:02 -0400

    Update platform detection

--- lib/src/flutter/platform.dart
+++ lib/src/flutter/platform.dart
@@ -1,30 +1,22 @@
-import 'dart:io';
-
 import 'package:flutter/foundation.dart';
 
-bool get kIsAndroid => !kIsWeb && Platform.isAndroid;
-
-bool get kIsIOS => !kIsWeb && Platform.isIOS;
-
-bool get kIsMacOS => !kIsWeb && Platform.isMacOS;
-
-bool get kIsCupertino => !kIsWeb && (kIsIOS || kIsMacOS);
-
-/// Returns the current [TargetPlatform], or null for web or unknown platforms
+/// Returns the current [TargetPlatform], or null for web
 TargetPlatform? get kCurrentPlatform {
   if (kIsWeb) {
     return null;
   }
 
-  if (Platform.isAndroid) return TargetPlatform.android;
-  if (Platform.isFuchsia) return TargetPlatform.fuchsia;
-  if (Platform.isIOS) return TargetPlatform.iOS;
-  if (Platform.isLinux) return TargetPlatform.linux;
-  if (Platform.isMacOS) return TargetPlatform.macOS;
-  if (Platform.isWindows) return TargetPlatform.windows;
-  // Default to null for unknown platforms
-  return null;
+  return defaultTargetPlatform;
 }
+
+bool get kIsAndroid =>
+    !kIsWeb && defaultTargetPlatform == TargetPlatform.android;
+
+bool get kIsCupertino => !kIsWeb && (kIsIOS || kIsMacOS);
+
+bool get kIsIOS => !kIsWeb && defaultTargetPlatform == TargetPlatform.iOS;
+
+bool get kIsMacOS => !kIsWeb && defaultTargetPlatform == TargetPlatform.macOS;
 
 extension TargetPlatformExtension on TargetPlatform {
   bool get isCupertino =>
